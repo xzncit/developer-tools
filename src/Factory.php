d96706bd640d0acaecbdfd4d1450649427b30af3
xzncit <158373108@qq.com>
2022-08-09 09:55:33 +0800

    优化更亲

--- src/Factory.php
+++ src/Factory.php
@@ -23,7 +23,7 @@ class Factory {
      * Current version of program
      * @var string
      */
-    public static $version = "0.5.1";
+    public static $version = "0.5.2";
 
     /**
      * @param $name
