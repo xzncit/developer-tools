4c813bd15e64247ee7d5f79cdfb1992fd0333eb4
xzncit <158373108@qq.com>
2024-11-02 18:04:23 +0800

    v0.5.7

--- src/Factory.php
+++ src/Factory.php
@@ -23,7 +23,7 @@ class Factory {
      * Current version of program
      * @var string
      */
-    public static $version = "0.5.6";
+    public static $version = "0.5.7";
 
     /**
      * @param $name
